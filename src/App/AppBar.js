2f6033a1832a14723de562b5df033d34de4828c6
Stevo99 <Sfailla1983@gmail.com>
2019-09-16 21:00:10 -0400

    added AppBar component

---
+++ src/App/AppBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AppBarStyle = styled.div`
+	width: 100%;
+	height: 6rem;
+	color: black;
+	background-color: white;
+`;
+
+const AppBar = () => {
+	return (
+		<AppBarStyle>
+			<h3>AppBar</h3>
+		</AppBarStyle>
+	);
+};
+
+export default AppBar;
