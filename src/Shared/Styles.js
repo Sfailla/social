502d16a462b6ab7e20d59d6298ef07aa54ccc5fe
Stevo99 <Sfailla1983@gmail.com>
2019-09-17 13:11:57 -0400

    added more styles to shared styles folder

--- src/Shared/Styles.js
+++ src/Shared/Styles.js
@@ -2,7 +2,25 @@ import styled from 'styled-components';
 
 // const darkTheme = 'background-color: #333';
 
+export const MainBG = 'background-color: #282c34';
+export const GreenBlue = 'color: #00ffd0';
+
 export const Wrapper = styled.div`
 	max-width: 1200px;
 	margin: 0 auto;
 `;
+
+export const BoxShadow = {
+	sm: {
+		'box-shadow':
+			'0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)'
+	},
+	md: {
+		'box-shadow':
+			'0 8px 12px -3px rgba(0, 0, 0, 0.4), 0 4px 6px -2px rgba(0, 0, 0, 0.2)'
+	},
+	lg: {
+		'box-shadow':
+			'0 10px 15px -4px rgba(0, 0, 0, 0.3), 0 7px 7px -5px rgba(0, 0, 0, 0.2)'
+	}
+};
