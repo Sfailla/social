a5ade74a5c7d4c00ea3e8e3d9a97e86735dcb43e
Stevo99 <Sfailla1983@gmail.com>
2019-09-16 21:01:27 -0400

    minor changes to style

--- src/Shared/Styles.js
+++ src/Shared/Styles.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 // const darkTheme = 'background-color: #333';
 
 export const Wrapper = styled.div`
-	max-width: 1000px;
+	max-width: 1200px;
 	margin: 0 auto;
 `;
